8a96d24fa4f5149636529250e5b2c15aa7c7c842
francescoiannoni-cyber <francesco.iannoni@searchon.it>
2026-03-20 14:23:49 +0100

    Update python_arima.py

--- python_arima.py
+++ python_arima.py
@@ -19,7 +19,7 @@ OUTPUT_SHEET_NAME = 'Previsione_Output_SARIMAX'
 
 FORECAST_STEPS = 30
 RETRAIN_START_DATE = '2025-09-01'
-OUTLIER_DATE = pd.to_datetime('2026-02-28')
+OUTLIER_DATE = pd.to_datetime('2026-03-14')
 ORDER = (0, 1, 1)
 SEASONAL_ORDER = (0, 0, 1, 7)
 
