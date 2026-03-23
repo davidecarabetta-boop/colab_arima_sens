83802f1436b367ee90b8fecaaeddf551fe8d2c2c
francescoiannoni-cyber <francesco.iannoni@searchon.it>
2026-03-23 09:48:02 +0100

    Update python_prophet.py

--- python_prophet.py
+++ python_prophet.py
@@ -131,6 +131,7 @@ def run_prophet_forecast(df, steps):
         'Data': df_output['ds'].dt.strftime('%Y-%m-%d'),
         'Tipo': df_output['is_real'].map({True: 'REALE', False: 'PREVISIONE'}),
         'Dato_Finale': (df_output['Valore_Combinato'] / divisor).clip(lower=0).round(2),
+        'Dato reale': (pd.to_numeric(df['y'], errors='coerce') / divisor).round(2),
         'CI_Superiore': (pd.to_numeric(df_output['yhat_upper'], errors='coerce') / divisor).round(2),
         'CI_Inferiore': (pd.to_numeric(df_output['yhat_lower'], errors='coerce') / divisor).round(2)
     })
